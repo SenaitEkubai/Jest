853d69c24d6f94258901ccaa8fec75151543a24f
senaitekubai <senaitekubai@gmail.com>
2022-07-20 11:53:42 +0100

    test floating numbers

--- __tests__/functions.jest.js
+++ __tests__/functions.jest.js
@@ -53,3 +53,9 @@ test("2 plus 2 ", () => {
   expect(value).toBe(4);
   expect(value).toEqual(4);
 });
+
+// to test floating numbers
+test("adding floating numbers ", () => {
+  const value = 0.1 + 0.2;
+  expect(value).toBeCloseTo(0.3);
+});
