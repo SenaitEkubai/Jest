381f0bb998bc377e549c857357bc2bebb945cb40
senaitekubai <senaitekubai@gmail.com>
2022-07-20 11:53:12 +0100

    test using logical matcher

--- __tests__/functions.jest.js
+++ __tests__/functions.jest.js
@@ -42,3 +42,14 @@ test("obejct assignment", () => {
   data["two"] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
+
+// logical matchers
+test("2 plus 2 ", () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
