5450750ca5e4908c8f781c03ef97856c108dde52
senaitekubai <senaitekubai@gmail.com>
2022-07-20 11:38:10 +0100

    test if return equals

--- __tests__/functions.jest.js
+++ __tests__/functions.jest.js
@@ -5,3 +5,9 @@ const functions = require("../functions/functions");
 test("is defined", () => {
   expect(functions.getAge("Rudy", 35)).toBeDefined();
 });
+
+// test if return equals
+
+test("2 + 2 is equal 2 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
--- functions/functions.js
+++ functions/functions.js
@@ -2,5 +2,8 @@ const functions = {
   getAge: (name, age) => {
     return `${name} is ${age} years old`;
   },
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
 };
 module.exports = functions;
